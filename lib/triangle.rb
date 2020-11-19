9f6076cad0c2c3e6c0e4aa5a2d17a8677082a39e
Zac <zacharyflynn06@gmail.com>
2020-11-19 14:32:59 -0700

    commit tree c7a94a81a8a87ef147db6af9e7003fbb7eeb404f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    # @all = []
+    # @all << @s1
+    # @all << @s2
+    # @all << @s3
+    # @all.sort
+    if ((@s1 + @s2) <= @s3) || ((@s2 + @s3) <= @s1) || ((@s3 + @s1) <= @s2) || ((@s1 <= 0.0) || (@s2 <= 0.0) || (@s3 <= 0.0))
+      raise TriangleError
+    elsif (@s1 == @s2) && (@s2 == @s3)
+      :equilateral
+    elsif (@s1 == @s2) || (@s2 == @s3) || (@s1 == @s3)
+      :isosceles 
+    else
+      :scalene
+    end
+    # binding.pry
+  end
+
+  class TriangleError < StandardError
+    # def message
+    #   "not a valid triangle"
+    # end
+  end
+
 end
